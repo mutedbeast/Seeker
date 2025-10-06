9f0d63bad7c4471565812ca5bc2a790dd74b0073
mutedbeast <harshgkamble@gmail.com>
2025-10-06 11:40:40 +0530

    earlier custom used a file in project folder now its uses any file in system

--- seeker.py
+++ seeker.py
@@ -5,19 +5,18 @@ from without_argv import without_arg
 
 a = 'wordlist_all.txt'
 c = 'wordlist_common.txt'
-custom = 'wordlist_test.txt'
 help = False
 total_directories = 0
 
 def usage_guide():
     print("Usage : Python seeker.py <domain> <operation> <wordlist> \n Use all for full search, com for common search" )
 
-
 if len(sys.argv) == 1:
     without_arg()
 else:
     domain = sys.argv[1]
     operation = sys.argv[2]
+    wordlist_file_directory = sys.argv[3]
 
 if sys.argv[1] == '-help' or sys.argv[1] == '-h'or sys.argv[1] == 'help':
     help = True
@@ -28,7 +27,7 @@ if operation == '-a':
 elif operation == '-c':
     wordlist = wordlist(c)
 elif operation == '-custom':
-    wordlist = wordlist(custom)
+    wordlist = wordlist(wordlist_file_directory)
 
 if help == False:
     for words in wordlist:
@@ -37,7 +36,7 @@ if help == False:
             total_directories = total_directories + 1 
             print(f"{domain}/{words}")
         
-print(f"Total {total_directories} found")
+print(f"\nTotal {total_directories} found")
 
 
 
